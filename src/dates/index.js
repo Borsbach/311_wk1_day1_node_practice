7937fd7261ae3132affdd51863d2235c55783067
Alex Borsbach <alexborsbach@Alexs-MacBook-Pro.local>
2019-07-14 13:41:16 -0500

    final wk1 day1

--- src/dates/index.js
+++ src/dates/index.js
@@ -1,23 +1,29 @@
 // import moment here; use this package in each function
-const moment = require('moment');
+const moment = require("moment");
 
+//today
 const today = () => {
-  // write code for dates.today
+  const day = moment().format("dddd");
+  console.log("Today is: ",day);
+  return day;
+};
 
-}
-
+//calendar
 const calendar = () => {
-  // write code for dates.calendar
+  const date = moment().format("LL");
+  console.log("Today's date is:", date);
+  return date;
+};
 
-}
-
+//currentTime
 const currentTime = () => {
-  // write code for dates.currentTime
-
-}
+  const time = moment().format('hLTS')
+  console.log("The current time is: ", time)
+  return time
+};
 
 module.exports = {
   today,
   calendar,
   currentTime
-}
+};
--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,15 +1,28 @@
+//isEven
 const isEven = (num) => {
   if(num % 2 === 0) {
+    console.log("Even number is: ", num)
     return true
   }
 }
 
-function sum(arr){
+//Sum
+const sum = (arr) => {
   return (arr.length === 0) ? 0 : arr[0] + sum(arr.slice(1));
 }
 
-const comboSum = (arr, sum) => {
-  
+//comboSum
+function sumOf(arr){
+  return (arr.length === 0) ? 0 : arr[0] + sum(arr.slice(1));
+}
+
+const comboSum = (arr, num) => {
+  let newSum = sumOf(arr)
+  if(newSum === num) {
+    return true
+  } else {
+    return false
+  }
 }
 
 module.exports = {
